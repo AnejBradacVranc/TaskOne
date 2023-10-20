5ada4e7b8c56fe5313572bb1291230689c4c4a3c
Anej BV <mikedrifner@gmail.com>
2023-10-20 11:13:18 +0200

    Simplified generator. Used List constructor.

--- naloga01/src/main/java/com/example/podnaloga01/PlayersGenerator.kt
+++ naloga01/src/main/java/com/example/podnaloga01/PlayersGenerator.kt
@@ -7,35 +7,28 @@ class PlayersGenerator {
     companion object {
         fun generate(n: Int): List<Player> {
             val faker = Faker()
-            val players = mutableListOf<Player>()
             val random = Random.Default;
 
-            for (i in 1..n) {
+            return List(n){
                 val type = random.nextBoolean()
 
-                players.add(
-                    if (type) {
-                        Player(
-                            faker.money.amount(IntRange(100, 200), true, ".", ","),
-                            faker.name.name(),
-                            faker.name.lastName(),
-                            Helpers.generatePlayerRank(n)
-                        )
-                    }
-                    else {
-                        CompetitivePlayer(
-                            faker.money.amount(IntRange(100, 200), true, ".", ","),
-                            faker.name.name(),
-                            faker.name.lastName(),
-                            Helpers.generatePlayerRank(n),
-                            Helpers.generatePlayerRank(n),
-                            faker.money.amount(IntRange(100, 200), true, ".", ",")
-                        )
-                    }
-                )
+                if (type)
+                    Player(
+                        faker.money.amount(IntRange(100, 200), true, ".", ","),
+                        faker.name.name(),
+                        faker.name.lastName(),
+                        Helpers.generatePlayerRank(n)
+                    )
+                else
+                    CompetitivePlayer(
+                        faker.money.amount(IntRange(100, 200), true, ".", ","),
+                        faker.name.name(),
+                        faker.name.lastName(),
+                        Helpers.generatePlayerRank(n),
+                        Helpers.generatePlayerRank(n),
+                        faker.money.amount(IntRange(100, 200), true, ".", ",")
+                    )
             }
-
-            return players
         }
     }
 }
